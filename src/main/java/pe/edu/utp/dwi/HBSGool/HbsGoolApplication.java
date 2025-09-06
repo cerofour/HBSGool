f691d64f9c93f2df9c5e179a01e43a377105cff9
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-09-06 11:43:22 -0500

    Commit inicial

---
+++ src/main/java/pe/edu/utp/dwi/HBSGool/HbsGoolApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.dwi.HBSGool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbsGoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbsGoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/dwi/HBSGool/HbsGoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.dwi.HBSGool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbsGoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
