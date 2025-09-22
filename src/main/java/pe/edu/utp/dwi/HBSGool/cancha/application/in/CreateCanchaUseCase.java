06c28bf0dc85c90b889e4d654d765f17d97efb32
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-09-22 09:40:16 -0500

    YA NO USAREMOS HEXAGONAL!!!

--- src/main/java/pe/edu/utp/dwi/HBSGool/cancha/application/in/CreateCanchaUseCase.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.utp.dwi.HBSGool.cancha.application.in;
-
-import pe.edu.utp.dwi.HBSGool.cancha.domain.Cancha;
-
-public interface CreateCanchaUseCase {
-	Cancha createCancha(Cancha cancha);
-}
--- src/main/java/pe/edu/utp/dwi/HBSGool/cancha/infra/persistence/SpringDataCanchaRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.utp.dwi.HBSGool.cancha.infra.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SpringDataCanchaRepository extends JpaRepository<CanchaEntity, UUID> {
-}
