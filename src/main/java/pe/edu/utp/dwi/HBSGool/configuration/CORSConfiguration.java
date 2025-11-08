bb2f43bc36aefba05a436b1a17a28f9c3d350adc
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-11-08 12:26:03 -0500

    CORS configuration
    - allow all origins

---
+++ src/main/java/pe/edu/utp/dwi/HBSGool/configuration/CORSConfiguration.java
@@ -0,0 +1,27 @@
+package pe.edu.utp.dwi.HBSGool.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+						.allowedOrigins("*");
+			}
+		};
+	}
+}
